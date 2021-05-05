c7808263e78d7a4cce23774ae7d3bcfa0cb76d8e
Daniil <Daniil@DESKTOP-DGM97EK>
2021-05-05 14:47:31 +0300

    Добавьте файлы проекта.

---
+++ palju/palju/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.Messaging;
+
+namespace palju
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonSend_Clicked(object sender, EventArgs e)
+        {
+            var emailMessenger = CrossMessaging.Current.EmailMessenger;
+            if (emailMessenger.CanSendEmail)
+            {
+                emailMessenger.SendEmail($"{picker.SelectedItem}", $"{EntrySubject}");
+            }
+            ButtonSend.Text = "Отправлено!";
+        }
+
+        private void randombutton_Clicked(object sender, EventArgs e)
+        {
+            if (picker.SelectedIndex == 0)
+            {
+                EntrySubject.Text = $"{picker}";
+            }
+            if (picker.SelectedIndex == 1)
+            {
+                EntrySubject.Text = $"{picker}";
+            }
+            if (picker.SelectedIndex == 2)
+            {
+                EntrySubject.Text = $"{picker}";
+            }
+            if (picker.SelectedIndex == 3)
+            {
+                EntrySubject.Text = $"{picker}";
+            }
+            if (picker.SelectedIndex == 4)
+            {
+                EntrySubject.Text = $"{picker}";
+            }
+        }
+    }
+}
